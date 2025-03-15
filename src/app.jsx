ccfe13d7e79a63cc7835ba3d623fa22e1ac1b1fc
jafar-iringallur <jafariringallur@gmail.com>
2025-03-15 10:56:24 +0530

    meta changes

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import 'src/global.css';
 import { useEffect } from 'react';
-
+import { Helmet } from 'react-helmet-async';
 import { useScrollToTop } from 'src/hooks/use-scroll-to-top';
 import useGA4 from 'src/hooks/useGA4';
 
@@ -11,6 +11,7 @@ import ThemeProvider from 'src/theme';
 
 export default function App() {
   const { sendPageView } = useGA4();
+  
   useEffect(() => {
     try {
       sendPageView(window.location.pathname + window.location.search);
@@ -20,10 +21,28 @@ export default function App() {
   }, [sendPageView]);
 
   useScrollToTop();
+  const {
+    VITE_APP_TITLE,
+    VITE_APP_DESCRIPTION,
+    VITE_APP_KEYWORDS,
+    VITE_APP_AUTHOR,
+    VITE_APP_OG_IMAGE,
+  } = import.meta.env;
 
   return (
+    <>
+     <Helmet>
+        <title>{VITE_APP_TITLE}</title>
+        <meta name="description" content={VITE_APP_DESCRIPTION} />
+        <meta name="keywords" content={VITE_APP_KEYWORDS} />
+        <meta name="author" content={VITE_APP_AUTHOR} />
+        <meta property="og:title" content={VITE_APP_TITLE} />
+        <meta property="og:description" content={VITE_APP_DESCRIPTION} />
+        <meta property="og:image" content={VITE_APP_OG_IMAGE} />
+      </Helmet>
     <ThemeProvider>
       <Router />
     </ThemeProvider>
+    </>
   );
 }
